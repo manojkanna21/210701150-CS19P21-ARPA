9daa91bd83d48af1363ccaa40102ef3ca478a8b2
manojkanna21 <130916413+manojkanna21@users.noreply.github.com>
2024-11-19 19:01:22 +0530

    Add files via upload

---
+++ 15-D-PythonExample/MultiplyTwoNumbers.py
@@ -0,0 +1,8 @@
+def MultiplyTwoNumbers():
+	a = 10
+	b = 20
+	res = a*b
+	return str(res)
+
+# c = MultiplyTwoNumbers()
+# print(c)
---
+++ 16-PythonPrimeNumber/IsPrime.py
@@ -0,0 +1,9 @@
+# import Math as m
+def IsPrime(n):
+    if (n==0 or n==1):
+        return "False"
+    for i in range(2, int(n*0.5)+1, 1):
+        if(n%i==0):
+            return "False"
+    return "True"
+
